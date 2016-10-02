33d9a092e8e7467ec45aeb87d1351c72d5190096
constantinosskitsas <constantinosskitsas@hotmail.com>
2016-10-02 23:04:05 +0300

    final

---
+++ Assigment1/src/Client.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+/**
+ * simulate the concurrent users while the request
+ * information form the server.Measures the latency
+ * of request
+ * 
+ * @author constantinos skitsas cskits01
+ *
+ */
+public class Client {
+
+	static Integer nextid = 1;
+	static ArrayList<Long> allLatency = new ArrayList<Long>();
+	public class SingleUser extends Thread {
+		private String ip;
+		private int port;
+		private int requestsToDo = 300;
+		private Socket userSocket;
+		private PrintWriter writer;
+		private BufferedReader reader;
+		
+		int myId = 0;
+		
+		public SingleUser(String ip,int port) {
+			this.ip=ip;
+			this.port=port;
+			synchronized (nextid) {
+				myId = nextid++;
+			}
+			establishConnection();
+		}
+
+		@Override
+		/** Wait server respond.After server response calculates latency
+		 * end when all request served.
+		*/
+		public void run() {
+			String messageRead="";
+			Boolean finishFlag=false;
+			long sendRequestTime = System.nanoTime();
+			long receiveAnswerTime = System.nanoTime();
+			long latency = 0;
+
+			try {
+				while (!finishFlag&&(messageRead = reader.readLine()) != null) {
+					receiveAnswerTime = System.nanoTime();
+					requestsToDo--;
+					if (requestsToDo == 0) {
+						reader.close();
+						writer.close();
+						this.userSocket.close();
+						latency = receiveAnswerTime - sendRequestTime;
+						synchronized(allLatency){
+						allLatency.add(latency);
+						}
+						finishFlag=true;
+					} else {
+						latency = receiveAnswerTime - sendRequestTime;
+						allLatency.add(latency);
+						writer.println("HELLO " + InetAddress.getLocalHost().getHostAddress() 
+								+ " " + port + " " + myId);
+						sendRequestTime = System.nanoTime();
+						writer.flush();
+					}
+				}
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+			Long Average=(long)0;
+			synchronized (allLatency) {
+				for (Long long1 : allLatency) {
+					Average=Average+long1/(long)Math.pow(10, 6);
+				}
+				Average=Average/allLatency.size();
+				System.out.println(Average);
+			}
+			
+
+		}
+	
+	/**
+	 * Establish Connection with the server
+	 */
+		private void establishConnection() {
+			try {
+				userSocket = new Socket(ip, port);
+				writer = new PrintWriter(userSocket.getOutputStream());
+				reader = new BufferedReader(new InputStreamReader(userSocket.getInputStream()));
+				writer.println("HELLO " + InetAddress.getLocalHost().getHostAddress() 
+						+ " " + port + " " + myId);
+				System.out.println("akdsg;lf");
+				writer.flush();
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 20; i++) {
+			Client.SingleUser mUser = new Client().new SingleUser(args[0],Integer.parseInt(args[1]));
+			mUser.start();
+		}
+
+	}
+
+}
---
+++ Assigment1/src/Server.java
@@ -0,0 +1,133 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ * Wait user request,accepts and creates random payload and send back
+ * with a welcome message.Calculates throughput.
+ */
+
+public class Server {
+	private ServerSocket server;
+	
+	private static Integer requestsServed = 0;
+	public static final long ONESECOND=1000000000;
+	private static int port = 0;
+	private static Integer maxRequests = 0;
+	private static long start;
+	private long interval = ONESECOND;
+	/**
+	 * crates a randomPayload to send back to the user
+	 * @return
+	 */
+	public String randomPayload() {
+		int size = (int) (1700000 * Math.random() + 300000);
+		StringBuilder stringBuilder = new StringBuilder(size);
+		for (int i = 0; i < size; i++) {
+			stringBuilder.append('a');
+		}
+		return stringBuilder.toString();
+	}
+
+	
+	public class UserThread extends Thread {
+		BufferedReader reader;
+		PrintWriter writer;
+		Socket uSocket;
+/**Constructor
+ * 
+ * @param socketToUse
+ */
+		public UserThread(Socket socketToUse) {
+
+			try {
+				uSocket = socketToUse;
+				reader = new BufferedReader(new InputStreamReader(uSocket.getInputStream()));
+					requestsServed++;
+					maxRequests--;	
+				writer = new PrintWriter(uSocket.getOutputStream());
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
+	/**takes from string and finds id number of the user request
+	 * 
+	 * @param from
+	 * @return id
+	 */
+		public String findId(String from) {
+			String aString = "";
+			String[] sth=from.split(" ");
+			return sth[2];
+		}
+
+		@Override
+		/**
+		 * reads user message and responds also calculates throughput
+		 */
+		public void run() {
+			String messageReceived="";
+			String messageToSend="";
+			boolean flag=false;
+			try {
+				while (maxRequests>0 &&(messageReceived = reader.readLine()) != null) {
+					messageToSend="Welcome + Id:"+ findId(messageReceived)+randomPayload();
+					writer.println(messageToSend);
+						
+						maxRequests--;
+						requestsServed++;
+						if (maxRequests <= 0) {
+							if(!flag)
+							System.out.println(requestsServed);		
+							flag=true;
+							writer.close();
+							server.close();
+							reader.close();
+						}
+					if(!flag){	
+					if (interval < System.nanoTime() - start) {
+						System.out.println(requestsServed);
+						start = System.nanoTime();
+					}
+					
+					}
+					writer.flush();
+				}
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
+	}
+/**creates server socket and waits request from users
+ * 
+ */
+	public void startServer() {
+		boolean flag=false;
+		try {
+			 server = new ServerSocket(port);
+			while (true) {
+				Socket mySocket = server.accept();
+				if(!flag)
+					start = System.nanoTime();
+				UserThread aThread=new UserThread(mySocket);
+				aThread.start();
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	
+
+	public static void main(String[] args) {
+		port=Integer.parseInt(args[0]);
+		maxRequests=Integer.parseInt(args[1]);
+		Server rela = new Server();
+		rela.startServer();
+	}
+
+}
+
